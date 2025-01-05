b22c46cef2519f4f2d9ca1a1d14adfd28a886000
Ganpat Jangir <ganpatlaljangid7688@gmail.com>
2025-01-05 18:07:56 +0530

    Initial Commit

---
+++ AwesomeFeatures/AwesomeFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeFeaturesApp.swift
+//  AwesomeFeatures
+//
+//  Created by Ganpat Jangir on 05/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwesomeFeatures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AwesomeFeatures
+//
+//  Created by Ganpat Jangir on 05/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
